f47b3913e78d670f0f5725a2079d6040695ddace
Pepo <pepo.ospina@gmail.com>
2020-08-18 21:27:05 +0200

    kusama being read

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,30 @@
 import * as React from "react";
 import { hot } from "react-hot-loader";
+import { ApiPromise, WsProvider } from '@polkadot/api';
 
 const reactLogo = require("./../assets/img/react_logo.svg");
 import "./../assets/scss/App.scss";
 
-class App extends React.Component<Record<string, unknown>, undefined> {
+class App extends React.Component<Record<string, unknown>, any> {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      state: {}
+    }
+  }
+
+  async componentWillMount() {
+    const wsProvider = new WsProvider('wss://rpc.polkadot.io');
+    const api = await ApiPromise.create({ provider: wsProvider });
+    this.setState({ state: { genesisHash: api.genesisHash.toHex() } });
+  }
+
   public render() {
     return (
       <div className="app">
-        <h1>Hello World!</h1>
-        <p>Foo to the barz</p>
-        <img src={reactLogo.default} height="480" />
+        <h1>Kusama</h1>
+        <pre>{JSON.stringify(this.state, undefined, 2)}</pre>
       </div>
     );
   }

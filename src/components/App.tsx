7782bb37bd73cc5e6a499f912c58d2314812dfcf
Pepo <pepo.ospina@gmail.com>
2020-08-18 22:46:56 +0200

    getting councile mebers at block

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,25 +7,48 @@ import "./../assets/scss/App.scss";
 
 class App extends React.Component<Record<string, unknown>, any> {
 
+  blockInput: HTMLInputElement;
+
   constructor(props) {
     super(props);
     this.state = {
       result: {},
-      loading: false
+      loading: false,
+      blockNumber: 3351476
     }
   }
 
   async componentWillMount() {
+    this.load();
+  }
+
+  async load() {
+    const blockNumber = this.state.blockNumber;
     this.setState({ loading: true });
-    const wsProvider = new WsProvider('wss://rpc.polkadot.io');
+    const wsProvider = new WsProvider('wss://kusama-rpc.polkadot.io');
     const api = await ApiPromise.create({ provider: wsProvider });
-    this.setState({ result: { genesisHash: api.genesisHash.toHex() }, loading: false });
+    const blockHash = await api.rpc.chain.getBlockHash(blockNumber)
+    const timestamp = await api.query.timestamp.now.at(blockHash);
+    const date = new Date((timestamp as any)*1000);
+    this.setState({ result: { 
+      genesisHash: api.genesisHash.toHex(),
+      now: date.toDateString(),
+      chain: await api.rpc.system.chain(),
+      address: await api.query.system.account.at(blockHash, 'DqpNAWBTgPQFETepyPvigwrY6eTxtKS6obajvai8h7G18pn'),
+      members: await api.query.council.members.at(blockHash)
+    }, loading: false });
+  }
+
+  blockChanged(e: any) {
+    this.setState({ blockNumber: e.target.value })
   }
 
   public render() {
     return (
       <div className="app">
         <h1>Kusama</h1>
+        <input value={this.state.blockNumber} onChange={evt => this.blockChanged(evt)}></input>
+        <button onClick={() => this.load()}>update</button>
         { this.state.loading ? (<p>Loading...</p>) : (<pre>{JSON.stringify(this.state.result, undefined, 2)}</pre>) }
       </div>
     );
